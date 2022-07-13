d338871eb0062b584508ed537a027599c443e2d8
Jose Herrera <jooherrera4@gmail.com>
2022-07-13 09:36:55 -0300

    Se agrega swagger -- swagger-ui/index.html

---
+++ src/main/java/com/example/demo/swagger/SwaggerConfig.java
@@ -0,0 +1,30 @@
+package com.example.demo.swagger;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import springfox.documentation.swagger.web.InMemorySwaggerResourcesProvider;
+import springfox.documentation.swagger.web.SwaggerResource;
+import springfox.documentation.swagger.web.SwaggerResourcesProvider;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Primary
+    @Bean
+    public SwaggerResourcesProvider swaggerResourcesProvider(InMemorySwaggerResourcesProvider defaultResourcesProvider) {
+        return () -> {
+            SwaggerResource wsResource = new SwaggerResource();
+            wsResource.setName("API V1");
+            wsResource.setSwaggerVersion("3.0");
+            wsResource.setLocation("/swagger.json");
+
+            List<SwaggerResource> resources = new ArrayList<>();
+            resources.add(wsResource);
+            return resources;
+        };
+    }
+
+}
